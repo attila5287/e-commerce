762f5f7bb72af8b030080c7687c8e0340a45d1da
attila5287 <atiturkoz@hotmail.com>
2021-03-30 04:56:30 -0600

    img urls on landing

--- routes/landing.js
+++ routes/landing.js
@@ -1,37 +1,42 @@
 const landing = [
 	{
-		"api/categories" : [
+		"api/categories": [
 			{
 				method: "GET",
 				route: "/api/categories",
 				description:
 					"find all categories, be sure to include its associated Products",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/cat_get_all.JPG"
 			},
 			{
 				method: "GET",
 				route: "/api/categories/:id",
 				description:
 					"find one category by its id value, be sure to include its associated Products",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/cat_get_one.JPG"
 			},
 			{
 				method: "POST",
 				route: "/api/categories/",
 				description: "create a new category",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/cat_post.JPG"
 			},
 			{
 				method: "PUT",
 				route: "/api/categories/:id",
 				description: "update a category by its id value",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/cat_put.JPG"
 			},
 			{
 				method: "DELETE",
 				route: "/api/categories/:id",
 				description: "delete a category by its id value",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/cat_delete.JPG"
 			}
 		]
 	},
@@ -42,32 +47,37 @@ const landing = [
 				route: "/api/tags/",
 				description:
 					"find all tags be sure to include its associated Product data",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/tag_get_all.JPG"
 			},
 			{
 				method: "GET",
 				route: "/api/tags/:id",
 				description:
 					"find a single tag by its id be sure to include its associated Product data",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/tag_get_one.JPG"
 			},
 			{
 				method: "POST",
 				route: "/api/tags/",
 				description: "create a new tag",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/tag_post.JPG"
 			},
 			{
 				method: "PUT",
 				route: "/api/tags/:id",
 				description: "update a tags name by its id value",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/tag_put.JPG"
 			},
 			{
 				method: "DELETE",
 				route: "/api/tags/:id",
 				description: " delete on tag by its id value",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/tag_delete.JPG"
 			}
 		]
 	},
@@ -78,32 +88,37 @@ const landing = [
 				route: "/api/products/",
 				description:
 					"find all products,be sure to include its associated Category and Tag data",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/pro_get_all.JPG"
 			},
 			{
 				method: "GET",
 				route: "/api/products/:id",
 				description:
 					"find a single product by its id, be sure to include its associated Category and Tag data",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/pro_get_one.JPG"
 			},
 			{
 				method: "POST",
 				route: "/api/products/",
 				description: "create new product",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/pro_post.JPG"
 			},
 			{
 				method: "PUT",
 				route: "/api/products/:id",
 				description: "update product",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/pro_put.JPG"
 			},
 			{
 				method: "DELETE",
 				route: "/api/products/:id",
 				description: "delete one product by its id value",
-				img_url: ""
+				img_url:
+					"https://raw.githubusercontent.com/attila5287/e-commerce/main/Assets/pro_delete.JPG"
 			}
 		]
 	}
